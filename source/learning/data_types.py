da077bab05b834a597eed9727cbd2377a266a31b
Rosemberg Porras <rosemberg.porras@bold.co>
2023-04-08 21:33:34 -0500

    feat: added learning scrips

---
+++ source/learning/data_types.py
@@ -0,0 +1,17 @@
+'''
+Data types
+'''
+
+'''
+Numeric types: int, float, complex
+'''
+
+int_variable=100
+
+float_variable=99.9
+
+complex_variable=82j
+
+print("a"+type(int_variable))
+print("b"+type(float_variable))
+print("c"+type(complex_variable))
---
+++ source/learning/variables.py
@@ -0,0 +1,19 @@
+'''
+Declaration of variables
+'''
+
+car="mcqueen"
+speed=43
+car_licence="hawaii"
+
+print(car)
+print(speed)
+print(car_licence)
+
+'''
+The space between variables is used with underscore
+The variables cannot contains only numbers
+The variables must have just numeric or alphabetic letters (or both)
+'''
+
+#This is also a comment
